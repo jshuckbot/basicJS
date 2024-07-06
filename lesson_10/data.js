34957f238515d84121ab9ae1cd333205bcf08ff4
jshuckbot <gorka_ivan@mail.ru>
2024-07-06 18:32:52 +0700

    ready lesson 10

---
+++ lesson_10/data.js
@@ -0,0 +1,20 @@
+const dataInfo = `[
+    {
+        "id": "ef6",
+        "url": "https://avatars.akamai.steamstatic.com/6130e7e424c5f6b14e1d4bf5ae508d6d10e10ae3_medium.jpg",
+        "width": "350",
+        "height": "450"
+    },
+    {
+        "id": "hh6",
+        "url": "https://obmenvsemfiles.net/fo/files_attachments/9b2/7f6/9b27f67ff4f3ec0cf5cc0c1e5e6da7aa.jpg",
+        "width": "300",
+        "height": "200"
+    },
+    {
+        "id": "ff2",
+        "url": "https://premprem.jp/wp-content/uploads/2017/11/gahag-010691.jpg",
+        "width": "400",
+        "height": "400"
+    }
+]`;
